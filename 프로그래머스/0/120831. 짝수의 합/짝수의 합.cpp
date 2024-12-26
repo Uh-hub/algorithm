def51488c354a8deb8e3533467da3f16e8573465
Noa <67996426+Uh-hub@users.noreply.github.com>
2024-12-26 10:13:53 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for(int i = 1; i <= n/2; i++){
+        answer += i*2;
+    }
+    return answer;
+}
