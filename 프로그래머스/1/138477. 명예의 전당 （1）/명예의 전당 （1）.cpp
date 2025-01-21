257036a2d7e6d2034408f80b27b78fca3c761ce7
Noa <67996426+Uh-hub@users.noreply.github.com>
2025-01-21 09:58:28 +0900

    [level 1] Title: 명예의 전당 (1), Time: 14.31 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
+    vector<int> rank;
+    for(int i = 0; i < score.size(); i++){
+        rank.push_back(score[i]);
+        sort(rank.begin(), rank.end(), greater());
+        
+        if(rank.size() < k){
+            answer.push_back(rank.back());
+        }
+        else{
+            answer.push_back(rank[k-1]);
+        }
+    }
+    return answer;
+}
