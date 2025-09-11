76f3acff6b92e4b4e1db14adac90cca1ab60de6e
KimYujin <67996426+Uh-hub@users.noreply.github.com>
2025-09-11 11:38:14 +0900

    Update 대충 만든 자판.cpp
    속도 개선한 코드 커밋

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.cpp
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.cpp
@@ -1,42 +1,47 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include <unordered_map>
 
 using namespace std;
 
 vector<int> solution(vector<string> keymap, vector<string> targets) {
     vector<int> answer;
+    unordered_map<char, int> found;
     
     for(string word : targets)
     {
-        
         int sum = 0;
         for(int i = 0; i < word.length(); i++)
         {
             int min_idx = 100;
-            for(int j = 0; j < keymap.size(); j++)
+            if(found.find(word[i]) != found.end())
             {
-                auto idx = keymap[j].find(word[i]);
-                if(idx != string::npos)
-                {
-                    min_idx = (idx + 1 >= min_idx ? min_idx : idx + 1);
-                }
-            }
-            if(min_idx == 100)
-            {
-                sum = -1;
-                break;
+                sum += found[word[i]];
             }
             else
             {
-                sum += min_idx;
+                for(int j = 0; j < keymap.size(); j++)
+                {
+                    auto idx = keymap[j].find(word[i]);
+                    if(idx != string::npos)
+                    {
+                        min_idx = (idx + 1 >= min_idx ? min_idx : idx + 1);
+                    }
+                }
+                if(min_idx == 100)
+                {
+                    sum = -1;
+                    break;
+                }
+                else
+                {
+                    found[word[i]] = min_idx;
+                    sum += min_idx;
+                }
             }
         }
         answer.push_back(sum);
     }
-    
-    
-    
     return answer;
 }
