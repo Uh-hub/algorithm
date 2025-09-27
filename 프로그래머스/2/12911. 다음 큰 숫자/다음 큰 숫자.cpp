ce5d01af8cbf985032682356b8d82fa648318502
KimYujin <67996426+Uh-hub@users.noreply.github.com>
2025-09-27 16:02:19 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.54 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -4,29 +4,8 @@
 using namespace std;
 
 int solution(int n) {
-    int answer = 0;
-    int n_cnt = 0;
-    int next_n_cnt = 0;
-    int tmp_n = n;
+    int cnt = __builtin_popcount(n);
+    while(cnt != __builtin_popcount(++n));
     
-    while(tmp_n > 0)
-        {
-            if(tmp_n % 2 == 1) n_cnt++;
-            tmp_n /= 2;
-        }
-    
-    while(n_cnt != next_n_cnt)
-    {
-        n++;
-        next_n_cnt = 0;
-        tmp_n = n;
-        
-        while(tmp_n > 0)
-        {
-            if(tmp_n % 2 == 1) next_n_cnt++;
-            tmp_n /= 2;
-        }
-    }
-    answer = n;
-    return answer;
+    return n;
 }
